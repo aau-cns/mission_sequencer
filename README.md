deca0db1d88f8a0589f683dcc801b4ee3c375a98
Christian Brommer <christian.brommer@aau.at>
2020-11-30 13:25:24 +0100

    [ADD] allow use of relative waypoints
    tested in simulation

---
+++ README.md
@@ -0,0 +1,48 @@
+
+# Instructions to run waypoints code 1.1  
+Through this code the drone is moving through set points clarified below:  
+x=0, y=0, z=2  
+x=0, y=1, z=2    
+Then doing RTL and landing.  
+  
+### Steps for launching the node:  
+1- user@user_name:~$ roscore  
+2- user@user_name:~$ source /opt/ros/melodic/setup.bash  
+3- user@user_name:~$ roslaunch mavros px4.launch fcu_url:="udp://:14540@127.0.0.1:14557"  
+4- user@user_name:~/catkin_ws2$ catkin build amaze_waypoint_following  
+5- user@user_name:~/catkin_ws2$ source devel/setup.bash  
+6- user@user_name:~/catkin_ws2$ rosrun amaze_waypoint_following amaze_waypoint_following  
+  
+Now node is already started to run.  
+If you want to make a simulation into gazebo use the following command:  
+user@user_name:~/px4/Firmware$ make px4_sitl_default gazebo  
+  
+### Errors might be faced and solutions:  
+  
+- INFO [dataman] Unknown restart, data manager file './dataman' size is 11798680 bytes  
+INFO [simulator] Waiting for simulator to accept connection on TCP port 4560  
+**treat it with:**  
+sudo apt upgrade libignition-math2  
+----------------------------------------  
+- FAILED: external/Stamp/sitl_gazebo/sitl_gazebo-build  
+**treat it with:**  
+make clean  
+----------------------------------------  
+- RLException: roscore cannot run as another roscore/master is already running. Please kill other roscore/master processes before relaunching. The ROS_MASTER_URI is http://user_name:11311/ The traceback for the exception was written to the log file  
+**treat it with:**  
+killall -9 roscore  
+killall -9 rosmaster  
+----------------------------------------  
+- If you're in such a situation in which the terminal command is not ending with ctrl + c  
+**treat it with:**  
+ctrl + z  
+----------------------------------------  
+- [rospack] Error: package 'amaze_waypoint_following' not found  
+**treat it with:**  
+User_name:~/catkin_ws2$ source /opt/ros/melodic/setup.bash  
+User_name:~/catkin_ws2$ source devel/setup.bash  
+----------------------------------------  
+- ERROR: cannot launch node of type [mavros/mavros_node]: Cannot locate node of type  
+**treat it with:**  
+source /opt/ros/melodic/setup.bash  
+ls /opt/ros/melodic/lib/mavros // you should see mav components
--- src/amaze_waypoint_following.cpp
+++ src/amaze_waypoint_following.cpp
@@ -1,338 +1,186 @@
 /**
  * @file offb_node.cpp
- * @brief Offboard control example node, written with MAVROS version 0.19.x, PX4 Pro Flight
- * Stack and tested in Gazebo SITL
+ * @brief offboard example node, written with mavros version 0.14.2, px4 flight
+ * stack and tested in Gazebo SITL
  */
 
- //https://uenota.github.io/px4_teleop/px4__teleop__cmds_8hpp_source.html
-
-/*
-    TODO: - relative navigation from start.
-          - whole mission outline
-
-*/
-
-#include <ros/ros.h>
 #include <geometry_msgs/PoseStamped.h>
-#include <geometry_msgs/Twist.h>
+#include <geometry_msgs/TwistStamped.h> //velocity control
+#include <math.h>
 #include <mavros_msgs/CommandBool.h>
+#include <mavros_msgs/CommandTOL.h>
 #include <mavros_msgs/SetMode.h>
 #include <mavros_msgs/State.h>
-#include <mavros_msgs/CommandTOL.h>
-#include <mavros_msgs/CommandTOLRequest.h>
-#include <mavros_msgs/CommandTOLResponse.h>
-#include <mavros_msgs/PositionTarget.h>
-#include <geometry_msgs/Point.h>
-#include <geometry_msgs/Vector3.h>
-#include <stdio.h>
-#include <string>
-#include <vector>
-#include <sstream> //istringstream
-#include <iostream> // cout
-#include <fstream> // ifstream
-#include <math.h>
+#include <ros/ros.h>
 #include <tf2/LinearMath/Quaternion.h>
 #include <tf2_geometry_msgs/tf2_geometry_msgs.h>
-using namespace std;
-
-double l2_norm(vector<double> const& u)
-{
-    double accum = 0.;
-    for (int i = 0; i < u.size(); ++i)
-    {
-        accum += u[i] * u[i];
-    }
-    return sqrt(accum);
-}
-/**
- * Reads csv file into table, exported as a vector of vector of doubles.
- * @param inputFileName input file name (full path).
- * @return data as vector of vector of doubles.
- */
-vector< vector<double> > parse2DCsvFile(string inputFileName)
-{
-    vector<vector<double> > data;
-    ifstream inputFile(inputFileName);
-    int l = 0;
- 
-    while (inputFile) {
-        l++;
-        string s;
-        if (!getline(inputFile, s)) break;
-        if (s[0] != '#') {
-            istringstream ss(s);
-            vector<double> record;
- 
-            while (ss) {
-                string line;
-                if (!getline(ss, line, ','))
-                    break;
-                try {
-                    record.push_back(stof(line));
-                }
-                catch (const std::invalid_argument e) {
-                    cout << "NaN found in file " << inputFileName << " line " << l
-                         << endl;
-                    e.what();
-                }
-            }
- 
-            data.push_back(record);
-        }
-    }
- 
-    if (!inputFile.eof()) {
-        cerr << "Could not read file " << inputFileName << "\n";
-        __throw_invalid_argument("File not found.");
-    }
- 
-    return data;
-}
 
 mavros_msgs::State current_state;
-void state_cb(const mavros_msgs::State::ConstPtr& msg){
-    current_state = *msg;
-}
+void state_cb(const mavros_msgs::State::ConstPtr &msg) { current_state = *msg; }
+
+// position Subscriber
+
 geometry_msgs::PoseStamped current_pose;
 bool pose_ok(false);
-void current_cb(const geometry_msgs::PoseStamped::ConstPtr& msg){
-    current_pose = *msg;
-    pose_ok = true;
+void current_cb(const geometry_msgs::PoseStamped::ConstPtr &msg) {
+  current_pose = *msg;
+  pose_ok = true;
 }
 
-enum States { EXIT, READ, WAIT, SEND, CHECK };
+double flight_altitude = 1;
+int point_time = 15;
 
-int main(int argc, char **argv)
-{
-    bool hovering = false;
+int main(int argc, char *argv[]) {
 
-    ros::init(argc, argv, "offb_node");
-    ros::NodeHandle nh;
+  ros::init(argc, argv, "offb_node");
+  ros::NodeHandle nh; // class: ros::NodeHandle, object: nh
 
-    ros::Subscriber state_sub = nh.subscribe<mavros_msgs::State>
-            ("/mavros/state", 10, state_cb);
-    ros::Subscriber current_sub = nh.subscribe<geometry_msgs::PoseStamped>
-            ("/mavros/local_position/pose", 10, current_cb);
-            // ("/ov_pose", 10, current_cb);
-    ros::Publisher setpoint_pub = nh.advertise<mavros_msgs::PositionTarget>
-            ("/mavros/setpoint_raw/local", 10);
-    ros::Publisher local_pos_pub = nh.advertise<geometry_msgs::PoseStamped>
-            ("/mavros/setpoint_position/local", 10);
-    ros::Publisher vel_pub = nh.advertise<geometry_msgs::Twist>
-            ("/mavros/setpoint_velocity/cmd_vel_unstamped", 10);
-    ros::ServiceClient arming_client = nh.serviceClient<mavros_msgs::CommandBool>
-            ("/mavros/cmd/arming");
-    ros::ServiceClient landing_client = nh.serviceClient<mavros_msgs::CommandTOL>
-            ("/mavros/cmd/land");
-    ros::ServiceClient set_mode_client = nh.serviceClient<mavros_msgs::SetMode>
-            ("/mavros/set_mode");
+  ros::Subscriber state_sub =
+      nh.subscribe<mavros_msgs::State>("mavros/state", 10, state_cb);
+  ros::Publisher local_pos_pub = nh.advertise<geometry_msgs::PoseStamped>(
+      "mavros/setpoint_position/local", 10);
+  ros::Subscriber current_sub = nh.subscribe<geometry_msgs::PoseStamped>(
+      "/mavros/local_position/pose", 10, current_cb);
+  ros::ServiceClient arming_client =
+      nh.serviceClient<mavros_msgs::CommandBool>("mavros/cmd/arming");
+  ros::ServiceClient land_client =
+      nh.serviceClient<mavros_msgs::CommandTOL>("mavros/cmd/land");
+  ros::ServiceClient set_mode_client =
+      nh.serviceClient<mavros_msgs::SetMode>("mavros/set_mode");
 
-    //the setpoint publishing rate MUST be faster than 2Hz
-    ros::Rate rate(20.0);
+  // the setpoint publishing rate MUST be faster than 2Hz
+  ros::Rate rate(20.0);
 
-    // wait for FCU connection
-    while(ros::ok() && !current_state.connected){
-        ros::spinOnce();
-        rate.sleep();
-    }
+  // wait for FCU connection
+  while (ros::ok() && !current_state.connected) {
+    ros::spinOnce();
+    rate.sleep();
+    ROS_INFO("connecting to FCT...");
+  }
 
-    while (ros::ok() && !pose_ok)
-    {
-        ros::spinOnce();
-        rate.sleep();
-    }    
+  while (pose_ok == false) {
+    ros::spinOnce();
+    rate.sleep();
+    ROS_INFO("Wait for valid init pose...");
+  }
 
-    // geometry_msgs::PoseStamped pose;
-    // pose = current_pose;
-    // pose.pose.position.x += 0;
-    // pose.pose.position.y += 0;
-    // pose.pose.position.z += 1;
-    printf("Current Pose: [%1.2f, %1.2f, %1.2f, %1.2f]\n",current_pose.pose.position.x,current_pose.pose.position.y,current_pose.pose.position.z,current_pose.pose.orientation.z);
-    //send a few setpoints before starting
-    for(int i = 100; ros::ok() && i > 0; --i){
-        local_pos_pub.publish(current_pose);
-        ros::spinOnce();
-        rate.sleep();
-    }
+  geometry_msgs::PoseStamped init_pose(current_pose);
+  ROS_INFO_STREAM("Got init pose [x,y,z]: " << init_pose.pose.position.x << " "
+                                            << init_pose.pose.position.x << " "
+                                            << init_pose.pose.position.x);
 
-    mavros_msgs::SetMode offb_set_mode;
-    offb_set_mode.request.custom_mode = "OFFBOARD";
+  geometry_msgs::PoseStamped pose;
 
-    mavros_msgs::CommandBool arm_cmd;
-    arm_cmd.request.value = true;
+  pose.pose.position.x = 0 + init_pose.pose.position.x;
+  pose.pose.position.y = 0 + init_pose.pose.position.y;
+  pose.pose.position.z = flight_altitude + init_pose.pose.position.z;
 
-    mavros_msgs::CommandTOL land_cmd;
-    land_cmd.request.altitude = 0;
-    land_cmd.request.latitude = 0;
-    land_cmd.request.longitude = 0;
-    land_cmd.request.min_pitch = 0;
-    land_cmd.request.yaw = 0; 
+  // send a few setpoints before starting
+  for (int i = 100; ros::ok() && i > 0; --i) {
+    local_pos_pub.publish(pose);
+    ros::spinOnce();
+    rate.sleep();
+  }
 
-    ros::Time last_request = ros::Time::now();
+  mavros_msgs::SetMode offb_set_mode;
+  offb_set_mode.request.custom_mode = "OFFBOARD";
 
-    bool reached = false;
-    geometry_msgs::PoseStamped set_pose;
-    geometry_msgs::PoseStamped starting_pose;
+  mavros_msgs::CommandBool arm_cmd;
+  arm_cmd.request.value = true;
 
-    bool run_FSM = false;
+  mavros_msgs::CommandTOL land_cmd;
+  land_cmd.request.yaw = 0;
+  land_cmd.request.latitude = 0;
+  land_cmd.request.longitude = 0;
+  land_cmd.request.altitude = 0;
 
-    States current_state_FSM = READ;
+  ros::Time last_request = ros::Time::now();
 
-    vector<vector<double>> data = parse2DCsvFile("/home/chriboehm/workspaces/CNS/catkin_ws_amaze/trajectory.txt");
-    int i = 0;
-
-    starting_pose = current_pose;
-    set_pose = starting_pose;
-    ros::Time start_time = ros::Time::now();
-
-    bool exit_programm = false;
-
-    vector<double> vect_1;
-
-    while( ros::ok() && !exit_programm )
-    {   
-        // if( current_state.mode != "OFFBOARD" && (ros::Time::now() - last_request > ros::Duration(5.0)) )
-        // {
-        //     if( set_mode_client.call(offb_set_mode) && offb_set_mode.response.mode_sent )
-        //     {
-        //         ROS_INFO("Offboard enabled");
-        //     }
-        //     last_request = ros::Time::now();
-        // }   
-        // else
-        // {   
-        //     if( !current_state.armed && (ros::Time::now() - last_request > ros::Duration(5.0)) )
-        //     {
-        //         if( arming_client.call(arm_cmd) && arm_cmd.response.success )
-        //         {
-        //             ROS_INFO("Vehicle armed");
-        //             run_FSM = true;
-        //         }
-        //         last_request = ros::Time::now();
-        //     }    
-        // }
-        if( !current_state.armed &&
-            (ros::Time::now() - last_request > ros::Duration(5.0))){
-            if( arming_client.call(arm_cmd) &&
-                arm_cmd.response.success){
-                ROS_INFO("Vehicle armed");
-            }
-            last_request = ros::Time::now();
-        } else {
-            
-            if( current_state.mode != "OFFBOARD" &&
-            (ros::Time::now() - last_request > ros::Duration(5.0))){
-                if( set_mode_client.call(offb_set_mode) &&
-                    offb_set_mode.response.mode_sent){
-                    ROS_INFO("Offboard enabled");
-                    run_FSM = true;
-                }
-                last_request = ros::Time::now();
-            }
+  // change to offboard mode and arm
+  while (ros::ok() && !current_state.armed) {
+    if (current_state.mode != "OFFBOARD" &&
+        (ros::Time::now() - last_request > ros::Duration(5.0))) {
+      ROS_INFO(current_state.mode.c_str());
+      if (set_mode_client.call(offb_set_mode) &&
+          offb_set_mode.response.mode_sent) {
+        ROS_INFO("Offboard enabled");
+      }
+      last_request = ros::Time::now();
+    } else {
+      if (!current_state.armed &&
+          (ros::Time::now() - last_request > ros::Duration(5.0))) {
+        if (arming_client.call(arm_cmd) && arm_cmd.response.success) {
+          ROS_INFO("Vehicle armed");
         }
+        last_request = ros::Time::now();
+      }
+    }
+    local_pos_pub.publish(pose);
+    ros::spinOnce();
+    rate.sleep();
+  }
 
-        if ( run_FSM == true )
-        {
-            vect_1 = { set_pose.pose.position.x-current_pose.pose.position.x, 
-                    set_pose.pose.position.y-current_pose.pose.position.y,
-                    set_pose.pose.position.z-current_pose.pose.position.z };
-            
-            tf2::Quaternion q_curr( current_pose.pose.orientation.x,
-                                    current_pose.pose.orientation.y,
-                                    current_pose.pose.orientation.z,
-                                    current_pose.pose.orientation.w );
-            tf2::Matrix3x3 m_curr(q_curr);
-            double roll_curr, pitch_curr, yaw_curr;
-            m_curr.getRPY(roll_curr, pitch_curr, yaw_curr);
-            tf2::Quaternion q_set( set_pose.pose.orientation.x,
-                                   set_pose.pose.orientation.y,
-                                   set_pose.pose.orientation.z,
-                                   set_pose.pose.orientation.w );
-            tf2::Matrix3x3 m_set(q_set);
-            double roll_set, pitch_set, yaw_set;
-            m_set.getRPY(roll_set, pitch_set, yaw_set);
+  // go to the first waypoint
 
-            double e_yaw = yaw_curr-yaw_set;
+  pose.pose.position.x = 0 + init_pose.pose.position.x;
+  pose.pose.position.y = 0 + init_pose.pose.position.y;
+  pose.pose.position.z = flight_altitude + init_pose.pose.position.z;
 
-            printf("Current Pose: [%1.2f, %1.2f, %1.2f, %1.2f]\n",current_pose.pose.position.x,current_pose.pose.position.y,current_pose.pose.position.z,yaw_curr);
-            printf("Set Pose: [%1.2f, %1.2f, %1.2f, %1.2f]\n",set_pose.pose.position.x,set_pose.pose.position.y,set_pose.pose.position.z,yaw_set);
-            printf("Current State: %d\n",current_state_FSM);
-            printf("Current 2-Norm value: %f\n",l2_norm(vect_1));
-            printf("Current yaw difference: %f\n",e_yaw);
-
-            tf2::Quaternion q_1,q_2;
-            tf2::convert(starting_pose.pose.orientation , q_1);
-            tf2::Matrix3x3 m_q_1(q_1);                       
-            double roll_q_1, pitch_q_1, yaw_q_1;
-            m_q_1.getRPY(roll_q_1, pitch_q_1, yaw_q_1);
-
-            switch (current_state_FSM)
-            {
-                case READ:
-                    if ( i == static_cast<int>(data.size()) )
-                    {
-                        printf("No Waypoints left.\n");
-                        current_state_FSM = EXIT;
-                        break;
-                    } 
-                    start_time = ros::Time::now();
-                    current_state_FSM = WAIT;
-                    break;
-                case WAIT:
-                    if ( ros::Time::now() - start_time > ros::Duration(data.at(i).at(0)) )
-                    {
-                        current_state_FSM = SEND;
-                    }                    
-                    break;
-                case SEND:
-                    printf("Waypoints sent.\n");
-                    set_pose.pose.position.x = starting_pose.pose.position.x + data.at(i).at(1);
-                    set_pose.pose.position.y = starting_pose.pose.position.y + data.at(i).at(2);
-                    set_pose.pose.position.z = starting_pose.pose.position.z + data.at(i).at(3);      
-                    q_2.setRotation(tf2::Vector3(0,0,1),data.at(i).at(4)+yaw_q_1);
-                    q_2.normalize();
-                    set_pose.pose.orientation.w = q_2[3];
-                    set_pose.pose.orientation.x = q_2[0];
-                    set_pose.pose.orientation.y = q_2[1];
-                    set_pose.pose.orientation.z = q_2[2];
-                    current_state_FSM = CHECK;
-                    break;
-                case CHECK:
-                    if ( l2_norm(vect_1) < 0.1 && e_yaw < 0.002 )
-                    {
-                        printf("Waypoints reached.\n");
-                        i++;
-                        current_state_FSM = READ;
-                    }
-                    break;                
-                default:
-                    set_pose.pose.position.x = starting_pose.pose.position.x;
-                    set_pose.pose.position.y = starting_pose.pose.position.y;
-                    set_pose.pose.position.z = starting_pose.pose.position.z;
-                    set_pose.pose.orientation.w = starting_pose.pose.orientation.w;
-                    set_pose.pose.orientation.x = starting_pose.pose.orientation.x;
-                    set_pose.pose.orientation.y = starting_pose.pose.orientation.y;
-                    set_pose.pose.orientation.z = starting_pose.pose.orientation.z;
-                    if ( l2_norm(vect_1) < 0.2 )
-                    {
-                        if( landing_client.call(land_cmd) && land_cmd.response.success )
-                        {
-                            ROS_INFO("Vehicle landed");
-                            run_FSM = false;
-                            exit_programm = true;
-                        }
-                        last_request = ros::Time::now();
-                    }
-                    break;
-            }
-        }
-        
-        set_pose.header.stamp = ros::Time::now();
-        local_pos_pub.publish(set_pose);
-
-        ros::spinOnce();
-        rate.sleep();
+  ROS_INFO("going to the first way point");
+  for (int i = 0; ros::ok() && i < point_time * 20; ++i) {
+    local_pos_pub.publish(pose);
+    if (abs(pose.pose.position.x - current_pose.pose.position.x) < 0.2 &&
+        abs(pose.pose.position.y - current_pose.pose.position.y) < 0.2 &&
+        abs(pose.pose.position.z - current_pose.pose.position.z) < 0.2) {
+      ROS_INFO("first way point got acheaved!");
     }
 
-    return 0;
+    ros::spinOnce();
+    rate.sleep();
+  }
+
+  // go to the second waypoint
+
+  pose.pose.position.x = 0 + init_pose.pose.position.x;
+  pose.pose.position.y = 1 + init_pose.pose.position.y;
+  pose.pose.position.z = flight_altitude + init_pose.pose.position.z;
+
+  // send setpoints for point_time seconds
+  ROS_INFO("going to second way point");
+  for (int i = 0; ros::ok() && i < point_time * 20; ++i) {
+    local_pos_pub.publish(pose);
+    if (abs(pose.pose.position.x - current_pose.pose.position.x) < 0.2 &&
+        abs(pose.pose.position.y - current_pose.pose.position.y) < 0.2 &&
+        abs(pose.pose.position.z - current_pose.pose.position.z) < 0.2) {
+      ROS_INFO("second way point got acheaved!");
+    }
+    ros::spinOnce();
+    rate.sleep();
+  }
+
+  // go back to the first waypoint
+  pose.pose.position.x = 0 + init_pose.pose.position.x;
+  pose.pose.position.y = 0 + init_pose.pose.position.y;
+  pose.pose.position.z = flight_altitude + init_pose.pose.position.z;
+
+  ROS_INFO("going back to the first point!");
+  // send setpoints for point_time seconds
+  for (int i = 0; ros::ok() && i < point_time * 20; ++i) {
+    if (abs(pose.pose.position.x - current_pose.pose.position.x) < 0.2 &&
+        abs(pose.pose.position.y - current_pose.pose.position.y) < 0.2 &&
+        abs(pose.pose.position.z - current_pose.pose.position.z) < 0.2) {
+      ROS_INFO("first way point got acheaved!");
+    }
+    local_pos_pub.publish(pose);
+    ros::spinOnce();
+    rate.sleep();
+  }
+
+  ROS_INFO("tring to land");
+  while (!(land_client.call(land_cmd) && land_cmd.response.success)) {
+    ROS_INFO("tring to land");
+    ros::spinOnce();
+    rate.sleep();
+  }
+  return 0;
 }
