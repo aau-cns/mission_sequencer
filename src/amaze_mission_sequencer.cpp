1a79e97ead06638d1b7ec7544273dfabed9c3756
chriboehm@cns-nb10 <christoph.boehm@aau.at>
2021-07-21 17:46:32 +0200

    [ADD] Added "this->" in getFilenames(). Needs testing, does the response have the right IDs?

--- src/amaze_mission_sequencer.cpp
+++ src/amaze_mission_sequencer.cpp
@@ -111,7 +111,7 @@ bool AmazeMissionSequencer::getFilenames()
     XmlRpc::XmlRpcValue filepaths;
 
     // get filepaths
-    if (!nh_.getParam("/autonomy/missions/mission_" + std::to_string(missionID_) + "/filepaths", filepaths))
+    if (!nh_.getParam("/autonomy/missions/mission_" + std::to_string(this->missionID_) + "/filepaths", filepaths))
     {
         // [TODO] Manage error
         return false;
